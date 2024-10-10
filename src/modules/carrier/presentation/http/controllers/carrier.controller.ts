5bd9fbbc44461c9fb8050f0b10c410eb37fa65b8
quang-doan-tpv-clv <157874791+quang-doan-tpv-clv@users.noreply.github.com>
2024-10-10 18:05:48 +0700

    wip: test source

--- src/modules/carrier/presentation/http/controllers/carrier.controller.ts
+++ src/modules/carrier/presentation/http/controllers/carrier.controller.ts
@@ -1,21 +1,24 @@
 import { CreateCarrierCommand } from '@modules/carrier/application/use-cases/create-carrier/create-carrier.command';
 import { CreateCarrierResult } from '@modules/carrier/application/use-cases/create-carrier/create-carrier.result';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { PresentablePresenter } from '@shared/_common/helpers/presentable-presenter';
 import { Result } from '@shared/_common/utils/result';
+import { CreateCarrierRequest } from '../dtos/create-carrier.request';
 
 @Controller('/carriers')
 export class CarrierController {
     constructor(private readonly _commandBus: CommandBus) {}
 
     @Get('/create-carrier')
-    async createCarrier() {
+    async createCarrier(@Query() request: CreateCarrierRequest) {
+        console.log(request);
+
         const result: Result<CreateCarrierResult> =
             await this._commandBus.execute(
                 new CreateCarrierCommand({
-                    carrierCode: '1111',
-                    carrierName: '1111'
+                    carrierCode: request.carrierCode,
+                    carrierName: request.carrierName
                 })
             );
 
--- src/modules/carrier/presentation/http/dtos/create-carrier.request.ts
+++ src/modules/carrier/presentation/http/dtos/create-carrier.request.ts
@@ -1,5 +1,13 @@
-// TODO: Think about this
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateCarrierRequest {
-    code: string;
-    name: string;
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(10)
+    carrierCode: string;
+
+    @IsString()
+    @IsNotEmpty()
+    carrierName: string;
 }
